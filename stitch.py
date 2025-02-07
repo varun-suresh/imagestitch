758abd60659d07b2c3d548077779817db638fb06
Varun Suresh <fab.varun@gmail.com>
2025-02-07 07:50:31 -0800

    Stitcher class tested with an example

--- stitch.py
+++ stitch.py
@@ -6,9 +6,6 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-img1 = cv2.imread('images/hill/1.jpg')
-img2 = cv2.imread('images/hill/2.jpg')
-
 class Stitcher:
     def __init__(self, img1, img2):
         self.img1 = img1
@@ -18,8 +15,8 @@ class Stitcher:
     def stitch(self):
         orb = cv2.ORB_create()
 
-        kp1, des1 = orb.detectAndCompute(img1, None)
-        kp2, des2 = orb.detectAndCompute(img2, None)
+        kp1, des1 = orb.detectAndCompute(self.img1, None)
+        kp2, des2 = orb.detectAndCompute(self.img2, None)
 
         # create BFMatcher object
         bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
@@ -34,6 +31,14 @@ class Stitcher:
         srcPoints = np.array([ kp2[matches[i].trainIdx].pt for i in range(self.N)])
         M, mask = cv2.findHomography(srcPoints, dstPoints, cv2.RANSAC,5.0)
         print("Homography matrix: ", M)
-        warpedImage = cv2.warpPerspective(img2, M, (img1.shape[0]+img2.shape[0],img1.shape[0]))
-        warpedImage[0:img1.shape[0], 0:img1.shape[1]] = img1
+        warpedImage = cv2.warpPerspective(self.img2, M, (self.img1.shape[0]+self.img2.shape[0],self.img1.shape[0]))
+        warpedImage[0:self.img1.shape[0], 0:self.img1.shape[1]] = self.img1
         return warpedImage
+
+if __name__ == "__main__":
+    img1 = cv2.imread('images/hill/1.jpg')
+    img2 = cv2.imread('images/hill/2.jpg')
+    stitcher = Stitcher(img1, img2)
+    stitched_image = stitcher.stitch()
+    print(stitched_image.shape)
+
