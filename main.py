d3e34640d7a76c9505963c9a2c29cc4314dfc179
Varun Suresh <fab.varun@gmail.com>
2025-02-09 20:11:26 -0800

    Testing changes

--- main.py
+++ main.py
@@ -2,15 +2,23 @@ from typing import List, Dict
 from fastapi import FastAPI, UploadFile
 from fastapi.staticfiles import StaticFiles
 from PIL import Image
+from stitch import Stitcher
 import io
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.post("/stitch")
-def stitch_images(images: List[UploadFile]) -> Dict[str, int]:
-    for f in images:
+def stitch_images(files: List[UploadFile]) -> Dict[str, int]:
+    imgs = []
+    for f in files:
         img_bytes = f.file.read()
         img = Image.open(io.BytesIO(img_bytes))
         print(img.size)
-    return {"images": len(images)}
+        print(f"Gets here!")
+    print(f"Gets here!")
+    # stitcher = Stitcher(imgs[0], imgs[1])
+    # stitched_image = stitcher.stitch()
+    # Image.save("static/stitched.jpg")
+    return {"images": len(files)}
+    # return {"status": 200}
