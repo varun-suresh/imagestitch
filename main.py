0001f6a01f35a76000ff2d6bc457753e5d08f353
Varun Suresh <fab.varun@gmail.com>
2025-02-10 22:00:46 -0800

    Display preview images

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from typing import List, Dict
 from fastapi import FastAPI, UploadFile
 from fastapi.staticfiles import StaticFiles
+import numpy as np
+import cv2
 from PIL import Image
 from stitch import Stitcher
 import io
@@ -13,12 +15,9 @@ def stitch_images(files: List[UploadFile]) -> Dict[str, int]:
     imgs = []
     for f in files:
         img_bytes = f.file.read()
-        img = Image.open(io.BytesIO(img_bytes))
-        print(img.size)
-        print(f"Gets here!")
-    print(f"Gets here!")
-    # stitcher = Stitcher(imgs[0], imgs[1])
-    # stitched_image = stitcher.stitch()
-    # Image.save("static/stitched.jpg")
-    return {"images": len(files)}
-    # return {"status": 200}
+        imgs.append(Image.open(io.BytesIO(img_bytes)))
+    stitcher = Stitcher(np.array(imgs[0]), np.array(imgs[1]))
+    stitched_image = stitcher.stitch()
+    cv2.imwrite("static/stitched.jpg", stitched_image)
+    # return {"images": len(files)}
+    return {"status": 200}
